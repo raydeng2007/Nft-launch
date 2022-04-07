d7cda05cc12e319bbe6a3ee304342fcb0eaf2cad
raydeng2007 <r6deng@uwaterloo.ca>
2022-04-07 13:06:31 -0400

    update base url

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x8B2ccA1537cE31Bc81c1f207a30BD83E8d0615bc";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-launch-j33gtyvo9-raydeng2007.vercel.app/api/";
+const METADATA_URL = "https://nft-launch.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
