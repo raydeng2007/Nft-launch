172d0c2c6926b86a763c2630fbac2a4cc552aee9
raydeng2007 <r6deng@uwaterloo.ca>
2022-04-07 16:05:15 -0400

    change image url

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -3,7 +3,7 @@ export default function handler(req, res) {
   const tokenId = req.query.tokenId;
   // As all the images are uploaded on github, we can extract the images from github directly.
   const image_url =
-    "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
+    "https://gateway.pinata.cloud/ipfs/QmdRAAT8kHTVvxxPb4bU3eKft7KuKqK61U68P2CLARJhVU";
   // The api is sending back metadata for a Crypto Dev
   // To make our collection compatible with Opensea, we need to follow some Metadata standards
   // when sending back the response from the api
@@ -11,6 +11,6 @@ export default function handler(req, res) {
   res.status(200).json({
     name: "Crypto Dev #" + tokenId,
     description: "Crypto Dev is a collection of developers in crypto",
-    image: image_url + tokenId + ".svg",
+    image: image_url
   });
 }
